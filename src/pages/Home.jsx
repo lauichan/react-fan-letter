d1163ad840786a8a786836736f376c672c7e16c2
lauichan <te06230623@gmail.com>
2024-01-30 19:45:38 +0900

    불필요한 컴포넌트 제거

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
-import FanLetter from "components/FanLetter";
+import FanLetterForm from "components/FanLetterForm";
+import FanLetterList from "components/FanLetterList";
+import MemberCard from "components/MemberCard";
+import { FanLetterContext } from "context/FanLetterContext";
+import { useContext } from "react";
 
 function Home() {
-  return <FanLetter />;
+  const { comments, member } = useContext(FanLetterContext);
+  const commentList = comments.filter((letter) => letter.writedTo === member);
+
+  return (
+    <>
+      <MemberCard />
+      <FanLetterForm />
+      <FanLetterList list={commentList} />
+    </>
+  );
 }
 
 export default Home;
