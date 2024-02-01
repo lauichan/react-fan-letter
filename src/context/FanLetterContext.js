345aa4b678a028a90fc0ab4a4b82b1d50c793713
lauichan <te06230623@gmail.com>
2024-02-01 11:32:21 +0900

    Context 파일 합치기

--- src/context/FanLetterContext.js
+++ src/context/FanLetterContext.js
@@ -1,3 +1,36 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { fanLetters } from "static/data";
 
 export const FanLetterContext = createContext(null);
+
+export function FanLetter({ children }) {
+  const [member, setMember] = useState("카리나");
+  const [comments, setComments] = useState(fanLetters);
+
+  const selectMember = (name) => {
+    setMember(name);
+  };
+
+  const addFanLetter = (newComment) => {
+    setComments((prev) => [...prev, newComment]);
+  };
+
+  const updateFanLetter = (updated) => {
+    setComments((prev) => prev.map((comment) => (comment.id === updated.id ? updated : comment)));
+  };
+
+  const deleteFanLetter = (id) => {
+    setComments((prev) => prev.filter((comment) => comment.id !== id));
+  };
+
+  const value = {
+    member,
+    comments,
+    selectMember,
+    addFanLetter,
+    updateFanLetter,
+    deleteFanLetter,
+  };
+
+  return <FanLetterContext.Provider value={value}>{children}</FanLetterContext.Provider>;
+}
