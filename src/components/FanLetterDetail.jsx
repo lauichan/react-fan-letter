4be0cacbdb7c6a733078ce67ace6b861cfd99bb2
lauichan <te06230623@gmail.com>
2024-02-01 19:43:48 +0900

    테스트

--- src/components/FanLetterDetail.jsx
+++ src/components/FanLetterDetail.jsx
@@ -1,9 +1,3 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
-import FanLetterForm from "./FanLetterForm";
-import { useDispatch, useSelector } from "react-redux";
-import { deleteFanLetter, updateFanLetter } from "store/modules/fanletter";
-import { selectMember } from "store/modules/member";
 import {
   ArticleBottomStyle,
   ArticleContentStyle,
@@ -23,66 +17,7 @@ function timeString(date) {
   });
 }
 
-function FanLetterDetail() {
-  const { id } = useParams();
-  const comments = useSelector((state) => state.fanletter);
-  const article = comments.find((letter) => letter.id === id) ?? {};
-  const [editMode, setEditMode] = useState(false);
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const changeEditMode = (bool) => {
-    setEditMode(bool);
-  };
-
-  const handleDeleteBtn = (id) => {
-    if (window.confirm("삭제 확인")) {
-      dispatch(deleteFanLetter(id));
-      navigate("/");
-    }
-  };
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    const { name, content, sendto } = e.target;
-
-    const isUnchanged =
-      name.value === article.nickname &&
-      content.value === article.content &&
-      sendto.value === article.writedTo;
-
-    if (isUnchanged) {
-      alert("수정된 내용이 없습니다.");
-      return;
-    }
-
-    const formData = {
-      id: article.id,
-      createdAt: article.createdAt,
-      avatar: article.avatar,
-      nickname: name.value,
-      content: content.value,
-      writedTo: sendto.value,
-    };
-
-    if (!window.confirm("팬레터 수정 확인")) return;
-
-    dispatch(updateFanLetter(formData));
-    dispatch(selectMember(formData.writedTo));
-    navigate("/");
-  };
-
-  if (editMode)
-    return (
-      <FanLetterForm
-        data={article}
-        handleOnSubmit={handleOnSubmit}
-        editMode={editMode}
-        changeEditMode={changeEditMode}
-      />
-    );
-
+function FanLetterDetail({ article, handleDeleteBtn, changeEditMode }) {
   return (
     <section>
       <ArticleStyle>
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,80 @@
 import FanLetterDetail from "components/FanLetterDetail";
+import FanLetterForm from "components/FanLetterForm";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteFanLetter, updateFanLetter } from "store/modules/fanletter";
+import { selectMember } from "store/modules/member";
 
 function Detail() {
-  return <FanLetterDetail />;
+  const { id } = useParams();
+  const comments = useSelector((state) => state.fanletter);
+  const article = comments.find((letter) => letter.id === id) ?? {};
+  const [editMode, setEditMode] = useState(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const changeEditMode = (bool) => {
+    setEditMode(bool);
+  };
+
+  const handleDeleteBtn = (id) => {
+    if (window.confirm("삭제 확인")) {
+      dispatch(deleteFanLetter(id));
+      navigate("/");
+    }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const { name, content, sendto } = e.target;
+
+    const isUnchanged =
+      name.value === article.nickname &&
+      content.value === article.content &&
+      sendto.value === article.writedTo;
+
+    if (isUnchanged) {
+      alert("수정된 내용이 없습니다.");
+      return;
+    }
+
+    const formData = {
+      id: article.id,
+      createdAt: article.createdAt,
+      avatar: article.avatar,
+      nickname: name.value,
+      content: content.value,
+      writedTo: sendto.value,
+    };
+
+    if (!window.confirm("팬레터 수정 확인")) return;
+
+    dispatch(updateFanLetter(formData));
+    dispatch(selectMember(formData.writedTo));
+    navigate("/");
+  };
+
+  if (editMode)
+    return (
+      <FanLetterForm
+        data={article}
+        handleOnSubmit={handleOnSubmit}
+        editMode={editMode}
+        changeEditMode={changeEditMode}
+      />
+    );
+
+  return (
+    <>
+      <FanLetterDetail
+        article={article}
+        changeEditMode={changeEditMode}
+        handleDeleteBtn={handleDeleteBtn}
+      />
+    </>
+  );
 }
 
 export default Detail;
